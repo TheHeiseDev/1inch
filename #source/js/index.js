b24ca8b8dc94688d19c5b879779c2aa88c8882ea
TheHeiseDev <meizuset@gmail.com>
2023-04-06 20:17:25 +0300

    Реализовал функционал табов, при клике на таб изменяется текст и изображение

--- #source/js/index.js
+++ #source/js/index.js
@@ -1,3 +1,20 @@
 //@prepros-append header.js
 //@prepros-append script.js
 //@prepros-append animation.js
+
+const tabItems = document.querySelectorAll(".wallet-block-tab");
+
+tabItems.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabItems.forEach((tab) => tab.classList.remove("tabs__caption_active")); // удаляю класс активности у всех табов на странице
+    tab.classList.add("tabs__caption_active"); // добавляю класс активности текущему табу
+  });
+});
+
+function changeTab(src, text) {
+  const welletTitle = document.querySelector(".wallet-block-text p");
+  const imageBlock = document.querySelector(".image__wrap img");
+
+  welletTitle.textContent = text;
+  imageBlock.src = src;
+}
--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 
+const tabItems = document.querySelectorAll(".wallet-block-tab");
+
+tabItems.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabItems.forEach((tab) => tab.classList.remove("tabs__caption_active")); // удаляю класс активности у всех табов на странице
+    tab.classList.add("tabs__caption_active"); // добавляю класс активности текущему табу
+  });
+});
+
+function changeTab(src, text) {
+  const welletTitle = document.querySelector(".wallet-block-text p");
+  const imageBlock = document.querySelector(".image__wrap img");
+
+  welletTitle.textContent = text;
+  imageBlock.src = src;
+}
+
 const body = document.querySelector("body");
 const logo = document.querySelector(".header__logo");
 
