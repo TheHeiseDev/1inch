f5aac95fbbcc6b91176b586143e3ba1228389574
TheHeiseDev <meizuset@gmail.com>
2023-04-14 12:49:11 +0300

    Настроил анимации через javascript

--- js/index.js
+++ js/index.js
@@ -85,35 +85,152 @@ const links = Array.from(menu.children);
 // }
 
 
-// ScrollReveal().reveal(".up-amim", {
-//   delay: 100,
-//   duration: 1000,
-// });
+ScrollReveal().reveal(".app-bellow", {
+  distance: "50px",
+  duration: 1500,
+  opacity: 0.5,
+  viewFactor: 0.3,
+});
+ScrollReveal().reveal(".app-bellow-mobile", {
+  distance: "50px",
+  duration: 1500,
+  desktop: true,
+  mobile: false,
+  viewFactor: 0.5,
+});
 
-// ScrollReveal().reveal(".app", {
-//   delay: 100,
-//   duration: 1000,
-// });
+// Animation module news.html
+ScrollReveal().reveal(".up", {
+  easing: "ease-in",
+});
 
-// ScrollReveal().reveal(".find", {
-//   delay: 100,
-//   duration: 1000,
-// });
-// ScrollReveal().reveal(".c1-anim", {
-//   delay: 100,
-//   duration: 1000,
-// });
+ScrollReveal().reveal(".up1", {
+  easing: "ease-in",
+  duration: 200,
+});
+ScrollReveal().reveal(".up2", {
+  easing: "ease-in",
+  duration: 300,
+});
+ScrollReveal().reveal(".up3", {
+  easing: "ease-in",
+  duration: 400,
+});
+ScrollReveal().reveal(".up4", {
+  easing: "ease-in",
+  duration: 500,
+});
+ScrollReveal().reveal(".up5", {
+  easing: "ease-in",
+  duration: 600,
+});
+ScrollReveal().reveal(".up6", {
+  easing: "ease-in",
+  duration: 700,
+});
+ScrollReveal().reveal(".up7", {
+  easing: "ease-in",
+  duration: 800,
+});
+ScrollReveal().reveal(".up8", {
+  easing: "ease-in",
+  duration: 900,
+});
+ScrollReveal().reveal(".up9", {
+  easing: "ease-in",
+  duration: 1000,
+});
+ScrollReveal().reveal(".up10", {
+  easing: "ease-in",
+  duration: 1100,
+});
 
-// ScrollReveal().reveal(".c2-anim", {
-//   delay: 200,
-//   duration: 1000,
-// });
+// Animation module wallet.html
+ScrollReveal().reveal(".rigth-in", {
+  delay: 0,
+  distance: "-100%",
+  duration: 1000,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  interval: 0,
+  opacity: 0.4,
+  origin: "right",
+  rotate: {
+    x: 0,
+    y: 0,
+    z: 0,
+  },
+  scale: 1,
+  cleanup: false,
+  container: document.documentElement,
+  desktop: false,
+  mobile: true,
+  reset: false,
+  useDelay: "always",
+  viewFactor: 0.3,
+});
+ScrollReveal().reveal(".left-in", {
+  delay: 0,
+  distance: "-100%",
+  duration: 1000,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  interval: 0,
+  opacity: 0.4,
+  origin: "left",
+  rotate: {
+    x: 0,
+    y: 0,
+    z: 0,
+  },
+  scale: 1,
+  cleanup: false,
+  container: document.documentElement,
+  desktop: false,
+  mobile: true,
+  reset: false,
+  useDelay: "always",
+  viewFactor: 0.3,
+});
+ScrollReveal().reveal(".bottom-in", {
+  delay: 0,
+  distance: "-50%",
+  duration: 1000,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  interval: 0,
+  opacity: 0.5,
+  origin: "top",
+  rotate: {
+    x: 0,
+    y: 0,
+    z: 0,
+  },
+  scale: 1,
+  cleanup: false,
+  container: document.documentElement,
+  desktop: true,
+  mobile: true,
+  reset: false,
+  useDelay: "always",
+  viewFactor: 0.5,
+});
 
-// ScrollReveal().reveal(".c3-anim", {
-//   delay: 300,
-//   duration: 1000,
-// });
-// ScrollReveal().reveal(".baner", {
-//   delay: 100,
-//   duration: 1000,
-// });
+// Animation module products-block.html
+ScrollReveal().reveal(".upp1", {
+  easing: "ease-in",
+  duration: 300,
+  viewFactor: 0.3,
+});
+ScrollReveal().reveal(".upp2", {
+  easing: "ease-in",
+  duration: 300,
+  viewFactor: 0.3,
+});
+ScrollReveal().reveal(".upp3", {
+  easing: "ease-in",
+  duration: 300,
+  viewFactor: 0.3,
+});
+ScrollReveal().reveal(".upp4", {
+  easing: "ease-in",
+  duration: 300,
+  viewFactor: 0.3,
+});
